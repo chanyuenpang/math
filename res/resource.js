7b63dd5d084596af7e83d54fa5c244538f47aa15
Yop <yop@snack-studio.com>
2014-10-25 00:25:29 +0800

    first commit

---
+++ res/resource.js
@@ -0,0 +1,7 @@
+var res = {
+	/Users/chanyuenpang/Desktop/cocos2d-js-spritebuilder/veewo/Math/veewo/GenResource_app/__/__/res/ : "res//Users/chanyuenpang/Desktop/cocos2d-js-spritebuilder/veewo/Math/veewo/GenResource.app/../../res/"
+};
+var g_resources = [];
+for (var i in res) {
+	g_resources.push(res[i]);
+}
